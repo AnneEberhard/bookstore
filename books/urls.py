ca1cc4b21e928ce13541b728032123d39bf35ab5
AnneEberhard <anne.eberhard@gmx.net>
2024-06-18 18:28:26 +0200

    put works

--- books/urls.py
+++ books/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', BookListView.as_view(), name='book-list'),
     path('<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('create/', BookCreateView.as_view(), name='book-create'),
-    path('<int:pk>/edit/', BookUpdateView.as_view(), name='book-edit'),
-    path('<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('edit/<int:pk>/', BookUpdateView.as_view(), name='book-edit'),
+    path('delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
